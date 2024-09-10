b75422f3cf22c81868feef43960245b8ff646dae
Jhonatan-PS <jhonatanpulido1500@gmail.com>
2024-09-10 16:51:03 -0500

    Se descomento el tls

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ s3 = boto3.client('s3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name=os.getenv('AWS_REGION'),
-    #use_ssl=True 
+    use_ssl=True 
 )
 
 BUCKET_NAME = 'b4rb3r'
